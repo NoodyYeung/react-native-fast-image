8596bc6bafd159a0facbbc13de538cd4cfb1cc0a
NoodyYeung <yeungchunyin1996@gmail.com>
2018-01-25 16:18:58 +0800

    Update FastImageViewPackage.java

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
@@ -16,6 +16,12 @@ public class FastImageViewPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Collections.<NativeModule>singletonList(new FastImageViewModule(reactContext));
     }
+    
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
